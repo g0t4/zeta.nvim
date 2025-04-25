2f9b16dfd1e19054462af133cff1a56c4a5f6e53
Wes Higbee <wes.mcclure@gmail.com>
2025-04-25 15:15:18 -0500

    hints

---
+++ lua/zeta/helpers/hints.lua
@@ -0,0 +1,7 @@
+
+-- tmp type hints for my dump func (so I can avoid using :messages)
+---@type fun(...)
+_G.BufferDump = _G.BufferDump
+---@type fun(...)
+_G.BufferDumpAppend = _G.BufferDumpAppend
+
