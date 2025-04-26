92537ca5ea393f72200e67d1c0ae34794dc2aeef
Wes Higbee <wes.mcclure@gmail.com>
2025-04-26 07:42:05 -0500

    rearrange so its clear now where this is not used

--- lua/zeta/copied/diff.lua
+++ lua/zeta/copied/diff.lua
@@ -17,36 +17,6 @@ SKIP_SEPARATOR = true -- a constant
 IN             = "in"; OUT = "out"; SAME = "same" -- token statuses
 
 -----------------------------------------------------------------------------
--- Split a string into tokens.  (Adapted from Gavin Kistner's split on
--- http://lua-users.org/wiki/SplitJoin.
---
--- @param text           A string to be split.
--- @param separator      [optional] the separator pattern (defaults to any
---                       white space - %s+).
--- @param skip_separator [optional] don't include the sepator in the results.
--- @return               A list of tokens.
------------------------------------------------------------------------------
-local function split(text, separator, skip_separator)
-    separator = separator or "%s+"
-    local parts = {}
-    local start = 1
-    local split_start, split_end = text:find(separator, start)
-    while split_start do
-        table.insert(parts, text:sub(start, split_start - 1))
-        if not skip_separator then
-            table.insert(parts, text:sub(split_start, split_end))
-        end
-        start = split_end + 1
-        split_start, split_end = text:find(separator, start)
-    end
-    if text:sub(start) ~= "" then
-        table.insert(parts, text:sub(start))
-    end
-    return parts
-end
-
-
------------------------------------------------------------------------------
 -- Derives the longest common subsequence of two strings.  This is a faster
 -- implementation than one provided by stdlib.  Submitted by Hisham Muhammad.
 -- The algorithm was taken from:
@@ -129,6 +99,35 @@ local function format_as_html(tokens)
         end
     end
     return diff_buffer
+end
+
+-----------------------------------------------------------------------------
+-- Split a string into tokens.  (Adapted from Gavin Kistner's split on
+-- http://lua-users.org/wiki/SplitJoin.
+--
+-- @param text           A string to be split.
+-- @param separator      [optional] the separator pattern (defaults to any
+--                       white space - %s+).
+-- @param skip_separator [optional] don't include the sepator in the results.
+-- @return               A list of tokens.
+-----------------------------------------------------------------------------
+local function split(text, separator, skip_separator)
+    separator = separator or "%s+"
+    local parts = {}
+    local start = 1
+    local split_start, split_end = text:find(separator, start)
+    while split_start do
+        table.insert(parts, text:sub(start, split_start - 1))
+        if not skip_separator then
+            table.insert(parts, text:sub(split_start, split_end))
+        end
+        start = split_end + 1
+        split_start, split_end = text:find(separator, start)
+    end
+    if text:sub(start) ~= "" then
+        table.insert(parts, text:sub(start))
+    end
+    return parts
 end
 
 -----------------------------------------------------------------------------
