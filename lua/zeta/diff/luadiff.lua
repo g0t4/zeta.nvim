56299f003b7574a4618cb32a82439986ef0dc800
Wes Higbee <wes.mcclure@gmail.com>
2025-04-28 14:25:26 -0500

    replace my copy of lua.diff w/ original so I make sure I haven't modified anything material

--- lua/zeta/diff/luadiff.lua
+++ lua/zeta/diff/luadiff.lua
@@ -9,16 +9,46 @@
 --
 -- License: MIT/X, see http://sputnik.freewisdom.org/en/License
 -----------------------------------------------------------------------------
-require("zeta.helpers.dump")
-local M        = {}
 
-SKIP_SEPARATOR = true -- a constant
+module(..., package.seeall)
 
-IN             = "in"; OUT = "out"; SAME = "same" -- token statuses
+SKIP_SEPARATOR = true  -- a constant
+
+IN   = "in"; OUT  = "out"; SAME = "same"  -- token statuses
 
 -----------------------------------------------------------------------------
--- Derives the longest common subsequence of two tokenized strings.  This is a faster
--- implementation than one provided by stdlib.  Submitted by Hisham Muhammad.
+-- Split a string into tokens.  (Adapted from Gavin Kistner's split on
+-- http://lua-users.org/wiki/SplitJoin.
+--
+-- @param text           A string to be split.
+-- @param separator      [optional] the separator pattern (defaults to any
+--                       white space - %s+).
+-- @param skip_separator [optional] don't include the sepator in the results.     
+-- @return               A list of tokens.
+-----------------------------------------------------------------------------
+function split(text, separator, skip_separator)
+   separator = separator or "%s+"
+   local parts = {}  
+   local start = 1
+   local split_start, split_end = text:find(separator, start)
+   while split_start do
+      table.insert(parts, text:sub(start, split_start-1))
+      if not skip_separator then
+         table.insert(parts, text:sub(split_start, split_end))
+      end
+      start = split_end + 1
+      split_start, split_end = text:find(separator, start)
+   end
+   if text:sub(start)~="" then
+      table.insert(parts, text:sub(start) )
+   end
+   return parts
+end
+
+
+-----------------------------------------------------------------------------
+-- Derives the longest common subsequence of two strings.  This is a faster
+-- implementation than one provided by stdlib.  Submitted by Hisham Muhammad. 
 -- The algorithm was taken from:
 -- http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_subsequence
 --
@@ -26,67 +56,41 @@ IN             = "in"; OUT = "out"; SAME = "same" -- token statuses
 -- @param t2             the second string.
 -- @return               the least common subsequence as a matrix.
 -----------------------------------------------------------------------------
-local function quick_LongestCommonSubsequence(old_tokens, new_tokens)
-    local num_old_tokens = #old_tokens
-    local num_new_tokens = #new_tokens
-    -- print("old_tokens: ", inspect(old_tokens))
-    -- print("new_tokens: ", inspect(new_tokens))
+function quick_LCS(t1, t2)
+   local m = #t1
+   local n = #t2
 
-    -- mt_tbl is a table that by default sets the value of every key to 0 on first use
-    --   table of zeros
-    local table_of_zeros = {
-        __index = function(t, k)
-            t[k] = 0
-            return 0
-        end
-    }
-    -- mt_C is a
-    --   table of table of zeros
-    --   * matrix of zeros
-    local matrix_of_zeros = {
-        __index = function(t, k)
-            local tbl = {}
-            setmetatable(tbl, table_of_zeros)
-            t[k] = tbl
-            return tbl
-        end
-    }
-    local matrix = {}
-    setmetatable(matrix, matrix_of_zeros)
-
-    -- what is super confusing for me is the offsets here...
-    -- they are doing weird shifting of cell values vs original token position...
-    -- it works out the same, assuming same offsets used in get_diff
-    -- but it is really confusing, so just heads up
-    -- one consequence is effectively seeing a non-existant nil token at the start of both old_tokens and new_tokens
-    -- thus the LCS count is overstated (i.e. 3 => 2 max len, 2 => 1 max len, etc)
-    --   b/c the nil at the start of each string is gonna match and count as a match that isn't there!
-    for i = 1, num_old_tokens + 1 do
-        -- for each old token:
-        local current_row = matrix[i]
-        local next_row = matrix[i + 1]
-        local old_token_hrm = old_tokens[i - 1]
-        -- print("i = " .. inspect(i) .. " = " .. inspect(old_token_hrm))
-        for j = 1, num_new_tokens + 1 do
-            -- for each new token:
-            local new_token_hrm = new_tokens[j - 1]
-            -- print("  j = " .. inspect(j) .. " = " .. inspect(new_token_hrm))
-
-            -- these cells are conditionally accessed
-            -- explicitly access them to ensure they're set to zero
-            -- so inspect sees all rows as lists (no gaps / implicit zeros)
-            current_row[j + 1] = current_row[j + 1]
-            current_row[j] = current_row[j]
-            next_row[j] = next_row[j]
-
-            if old_token_hrm == new_token_hrm then
-                next_row[j + 1] = current_row[j] + 1
-            else
-                next_row[j + 1] = math.max(next_row[j], current_row[j + 1])
-            end
-        end
-    end
-    return matrix
+   -- Build matrix on demand
+   local C = {}
+   local setmetatable = setmetatable
+   local mt_tbl = {
+      __index = function(t, k)
+         t[k] = 0
+         return 0
+      end
+   }
+   local mt_C = {
+      __index = function(t, k)
+         local tbl = {}
+         setmetatable(tbl, mt_tbl)
+         t[k] = tbl
+         return tbl
+      end
+   }
+   setmetatable(C, mt_C)
+   local max = math.max
+   for i = 1, m+1 do
+      local ci1 = C[i+1]
+      local ci = C[i]
+      for j = 1, n+1 do
+         if t1[i-1] == t2[j-1] then
+            ci1[j+1] = ci[j] + 1
+         else
+            ci1[j+1] = max(ci1[j], ci[j+1])
+         end
+      end
+   end
+   return C
 end
 
 
@@ -97,63 +101,34 @@ end
 -- @param text           The string to be escaped.
 -- @return               Escaped string.
 -----------------------------------------------------------------------------
-local function escape_html(text)
-    text = text:gsub("&", "&amp;"):gsub(">", "&gt;"):gsub("<", "&lt;")
-    text = text:gsub("\"", "&quot;")
-    return text
+function escape_html(text)
+   text = text:gsub("&", "&amp;"):gsub(">","&gt;"):gsub("<","&lt;")
+   text = text:gsub("\"", "&quot;")
+   return text
 end
 
 
 -----------------------------------------------------------------------------
 -- Formats an inline diff as HTML, with <ins> and <del> tags.
---
+-- 
 -- @param tokens         a table of {token, status} pairs.
 -- @return               an HTML string.
 -----------------------------------------------------------------------------
-local function format_as_html(tokens)
-    local diff_buffer = ""
-    local token, status
-    for _, token_record in ipairs(tokens) do
-        token = escape_html(token_record[1])
-        status = token_record[2]
-        if status == "in" then
-            diff_buffer = diff_buffer .. "<ins>" .. token .. "</ins>"
-        elseif status == "out" then
-            diff_buffer = diff_buffer .. "<del>" .. token .. "</del>"
-        else
-            diff_buffer = diff_buffer .. token
-        end
-    end
-    return diff_buffer
-end
-
------------------------------------------------------------------------------
--- Split a string into tokens.  (Adapted from Gavin Kistner's split on
--- http://lua-users.org/wiki/SplitJoin.
---
--- @param text           A string to be split.
--- @param separator      [optional] the separator pattern (defaults to any
---                       white space - %s+).
--- @param skip_separator [optional] don't include the sepator in the results.
--- @return               A list of tokens.
------------------------------------------------------------------------------
-local function split(text, separator, skip_separator)
-    separator = separator or "%s+"
-    local parts = {}
-    local start = 1
-    local split_start, split_end = text:find(separator, start)
-    while split_start do
-        table.insert(parts, text:sub(start, split_start - 1))
-        if not skip_separator then
-            table.insert(parts, text:sub(split_start, split_end))
-        end
-        start = split_end + 1
-        split_start, split_end = text:find(separator, start)
-    end
-    if text:sub(start) ~= "" then
-        table.insert(parts, text:sub(start))
-    end
-    return parts
+function format_as_html(tokens)
+   local diff_buffer = ""
+   local token, status
+   for i, token_record in ipairs(tokens) do
+      token = escape_html(token_record[1])
+      status = token_record[2]
+      if status == "in" then
+         diff_buffer = diff_buffer.."<ins>"..token.."</ins>"
+      elseif status == "out" then
+         diff_buffer = diff_buffer.."<del>"..token.."</del>"
+      else 
+         diff_buffer = diff_buffer..token
+      end
+   end
+   return diff_buffer
 end
 
 -----------------------------------------------------------------------------
@@ -164,86 +139,79 @@ end
 -- @param new             The "new" text string
 -- @param separator      [optional] the separator pattern (defaults ot any
 --                       white space).
--- @param skip_separator  Remove separator, don't include it in diff'd tokens
 -- @return               A list of annotated tokens.
 -----------------------------------------------------------------------------
-function M.diff(old, new, separator, skip_separator)
-    assert(old); assert(new)
-    new = split(new, separator, skip_separator)
-    old = split(old, separator, skip_separator)
-    -- print("new", inspect(new, true))
-    -- print("old", inspect(old, true))
+function diff(old, new, separator)
+   assert(old); assert(new)
+   new = split(new, separator); old = split(old, separator)
 
-    -- * remove common prefix/suffix
-    -- First, compare the beginnings and ends of strings to remove the common
-    -- prefix and suffix.  Chances are, there is only a small number of tokens
-    -- in the middle that differ, in which case  we can save ourselves a lot
-    -- in terms of LCS computation.
-    local prefix = "" -- common text in the beginning
-    local suffix = "" -- common text in the end
-    while old[1] and old[1] == new[1] do
-        local token = table.remove(old, 1)
-        table.remove(new, 1)
-        prefix = prefix .. token
-    end
-    while old[#old] and old[#old] == new[#new] do
-        local token = table.remove(old)
-        table.remove(new)
-        suffix = token .. suffix
-    end
+   -- First, compare the beginnings and ends of strings to remove the common
+   -- prefix and suffix.  Chances are, there is only a small number of tokens
+   -- in the middle that differ, in which case  we can save ourselves a lot
+   -- in terms of LCS computation.
+   local prefix = "" -- common text in the beginning
+   local suffix = "" -- common text in the end
+   while old[1] and old[1] == new[1] do
+      local token = table.remove(old, 1)
+      table.remove(new, 1)
+      prefix = prefix..token
+   end
+   while old[#old] and old[#old] == new[#new] do
+      local token = table.remove(old)
+      table.remove(new)
+      suffix = token..suffix
+   end
 
-    -- Setup a table that will store the diff (an upvalue for get_diff). We'll
-    -- store it in the reverse order to allow for tail calls.  We'll also keep
-    -- in this table functions to handle different events.
-    local rev_diff = {
-        put  = function(self, token, type) table.insert(self, { token, type }) end,
-        ins  = function(self, token) self:put(token, IN) end,
-        del  = function(self, token) self:put(token, OUT) end,
-        same = function(self, token) if token then self:put(token, SAME) end end,
-    }
+   -- Setup a table that will store the diff (an upvalue for get_diff). We'll
+   -- store it in the reverse order to allow for tail calls.  We'll also keep
+   -- in this table functions to handle different events.
+   local rev_diff = {
+      put  = function(self, token, type) table.insert(self, {token,type}) end,
+      ins  = function(self, token) self:put(token, IN) end,
+      del  = function(self, token) self:put(token, OUT) end,
+      same = function(self, token) if token then self:put(token, SAME) end end,
+   }
 
-    -- Put the suffix as the first token (we are storing the diff in the
-    -- reverse order)
+   -- Put the suffix as the first token (we are storing the diff in the
+   -- reverse order)
 
-    rev_diff:same(suffix)
+   rev_diff:same(suffix)
 
-    -- Define a function that will scan the LCS matrix backwards and build the
-    -- diff output recursively.
-    local function get_diff(C, old, new, i, j)
-        local old_i = old[i]
-        local new_j = new[j]
-        if i >= 1 and j >= 1 and old_i == new_j then
-            rev_diff:same(old_i)
-            return get_diff(C, old, new, i - 1, j - 1)
-        else
-            local Cij1 = C[i][j - 1]
-            local Ci1j = C[i - 1][j]
-            if j >= 1 and (i == 0 or Cij1 >= Ci1j) then
-                rev_diff:ins(new_j)
-                return get_diff(C, old, new, i, j - 1)
-            elseif i >= 1 and (j == 0 or Cij1 < Ci1j) then
-                rev_diff:del(old_i)
-                return get_diff(C, old, new, i - 1, j)
-            end
-        end
-    end
+   -- Define a function that will scan the LCS matrix backwards and build the
+   -- diff output recursively.
+   local function get_diff(C, old, new, i, j)
+      local old_i = old[i]
+      local new_j = new[j]
+      if i >= 1 and j >= 1 and old_i == new_j then
+         rev_diff:same(old_i)
+         return get_diff(C, old, new, i-1, j-1)
+      else
+         local Cij1 = C[i][j-1]
+         local Ci1j = C[i-1][j]
+         if j >= 1 and (i == 0 or Cij1 >= Ci1j) then
+            rev_diff:ins(new_j)
+            return get_diff(C, old, new, i, j-1)
+         elseif i >= 1 and (j == 0 or Cij1 < Ci1j) then
+            rev_diff:del(old_i)
+            return get_diff(C, old, new, i-1, j)
+         end
+      end
+   end
+   -- Then call it.
+   get_diff(quick_LCS(old, new), old, new, #old + 1, #new + 1)
 
-    -- Then call it.
-    local lcs_matrix = quick_LongestCommonSubsequence(old, new)
-    -- print("lcs:", inspect(lcs_matrix, true))
-    get_diff(lcs_matrix, old, new, #old + 1, #new + 1)
+   -- Put the prefix in at the end
+   rev_diff:same(prefix)
 
-    -- Put the prefix in at the end
-    rev_diff:same(prefix)
+   -- Reverse the diff.
+   local diff = {}
 
-    -- Reverse the diff.
-    local diff = {}
-
-    for i = #rev_diff, 1, -1 do
-        table.insert(diff, rev_diff[i])
-    end
-    diff.to_html = format_as_html
-    return diff
+   for i = #rev_diff, 1, -1 do
+      table.insert(diff, rev_diff[i])
+   end
+   diff.to_html = format_as_html
+   return diff
 end
 
-return M
+
+
