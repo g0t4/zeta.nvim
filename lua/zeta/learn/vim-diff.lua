90a964d38e9dc936c6d6013e066ccfe3e2084ab3
Wes Higbee <wes.mcclure@gmail.com>
2025-05-01 10:41:35 -0500

    ok these match so go with files

--- lua/zeta/learn/vim-diff.lua
+++ lua/zeta/learn/vim-diff.lua
@@ -4,22 +4,11 @@ local files = require("zeta.helpers.files")
 
 describe("vim.diff", function()
     it("tests vim.diff", function()
-        local zed_request_compare = files.read_example("01_request.json")
-        local zed_response_compare = files.read_example("01_response.json")
-
-        local zed_request =
-        '{"outline":"```lua/ask-openai/prediction/tests/calc/calc.lua\nfunction M.add\n```\n","input_events":"User edited \\"lua/ask-openai/prediction/tests/calc/calc.lua\\":\n```diff\n@@ -7,4 +7,5 @@\n \n \n \n+\n return M\n\n```\n\nUser edited \\"lua/ask-openai/prediction/tests/calc/calc.lua\\":\n```diff\n@@ -8,4 +8,5 @@\n \n \n \n+\n return M\n\n```\n\nUser edited \\"lua/ask-openai/prediction/tests/calc/calc.lua\\":\n```diff\n@@ -8,5 +8,4 @@\n \n \n \n-\n return M\n\n```","input_excerpt":"```ask-openai.nvim/lua/ask-openai/prediction/tests/calc/calc.lua\n<|start_of_file|>\n<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|user_cursor_is_here|>\n\n\n\nreturn M\n\n<|editable_region_end|>\n```","speculated_output":"<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\n<|user_cursor_is_here|>\n\n\n\nreturn M\n\n<|editable_region_end|>","can_collect_data":false,"diagnostic_groups":[]}'
-        local zed_response =
-        '{"output_excerpt":"<|editable_region_start|>\nlocal M = {}\n\nfunction M.add(a, b)\n    return a + b\nend\n\nfunction M.subtract(a, b)\n    return a - b\nend\n\nfunction M.multiply(a, b)\n    return a * b\nend\n\nfunction M.divide(a, b)\n    if b == 0 then\n        error(\\"Division by zero\\")\n    end\n    return a / b\nend\n\n\n\nreturn M\n\n<|editable_region_end|>\n```\n","request_id":"8319778f2ac147f7b1fbe1b0d5424132"}'
+        local zed_request = files.read_example("01_request.json")
+        local zed_response = files.read_example("01_response.json")
 
         local request_decoded = vim.json.decode(zed_request)
         local response_decoded = vim.json.decode(zed_response)
-
-        local zed_request_compare_decoded = vim.json.decode(zed_request_compare)
-        local zed_response_compare_decoded = vim.json.decode(zed_response_compare)
-        --
-        assert.are.same(zed_request_compare_decoded, request_decoded)
-        assert.are.same(zed_response_compare_decoded, response_decoded)
 
         -- BufferDumpAppend(request_decoded)
         -- BufferDumpAppend(response_decoded)
