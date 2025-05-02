1ce4d3511426d04789e1f503d898fc3888190c01
Wes Higbee <wes.mcclure@gmail.com>
2025-05-01 22:38:14 -0500

    reminder to try this

---
+++ lua/zeta/diff/wip/diff-match-patch.lua
@@ -0,0 +1,5 @@
+-- stumbled on this gem!
+--
+-- https://github.com/google/diff-match-patch/blob/master/lua/diff_match_patch.lua
+-- allegedly fast and implemented in many languages
+-- lots of test cases too
