32ac125c11cf59e2c54f25cc0123c05de274382f
Wes Higbee <wes.mcclure@gmail.com>
2025-05-05 08:58:10 -0500

    add excerpt class

---
+++ lua/zeta/predicts/Excerpt.lua
@@ -0,0 +1,18 @@
+---@class Excerpt
+---@field text string[]
+---@field start_line integer 0-indexed
+---@field end_line integer 0-indexed
+local Excerpt = {}
+Excerpt.__index = Excerpt
+
+---@param text string[]
+---@param start_line integer 0-indexed
+---@param end_line integer 0-indexed
+function Excerpt:new(text, start_line, end_line)
+    self.text = text
+    self.start_line = start_line
+    self.end_line = end_line
+    return self
+end
+
+return Excerpt
