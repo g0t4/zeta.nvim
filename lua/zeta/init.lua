015200f18817d1c72063c7a8d220e3296a3d93f1
Wes Higbee <wes.mcclure@gmail.com>
2025-05-08 23:56:40 -0500

    ok wait the issue was I had my statusline colors flipped, that was confusing me about it being enabled

--- lua/zeta/init.lua
+++ lua/zeta/init.lua
@@ -5,11 +5,10 @@ local M = {}
 
 function M.setup()
     config.setup()
-    -- FYI config is a hot mess, fix later
-    -- if config.is_enabled() then
-    --     poc.setup()
-    --     predictions.setup()
-    -- end
+    if config.is_enabled() then
+        poc.setup()
+        predictions.setup()
+    end
 end
 
 return M
